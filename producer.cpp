067f1bfa562b5e17baec784fb111c5d1e3e70dcd
shadyalattarr <shadyalattar00@gmail.com>
2024-12-04 05:02:47 +0200

    setting up for IPC and semaphores

--- producer.cpp
+++ producer.cpp
@@ -5,8 +5,51 @@
 #include <ctime>
 #include <unistd.h>
 #include <random>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <cstring>
 
 using namespace::std;
+
+void print_time()
+{
+    struct timespec tp;
+    if (clock_gettime(CLOCK_REALTIME, &tp) == -1) {
+        cerr << "Problem with retrieving time.\n";
+        exit(3);
+    }
+
+    time_t timestamp = time(NULL);
+    struct tm datetime = *localtime(&timestamp);
+    char output[50];
+    long milliseconds = tp.tv_nsec / 1000000; // turn ns to ms
+
+    // Format the date and time string
+    strftime(output, 50, "%m/%d/%Y %H:%M:%S", &datetime);
+
+    // Print using printf
+    printf("[%s.%03ld] ", output, milliseconds); // 0 padding
+}
+
+// Function to lock semaphore 
+void sem_Wait(int semid,short unsigned int sem_num) {
+    struct sembuf sb = {sem_num, -1, 0}; // Decrement semaphore sem_num
+    if (semop(semid, &sb, 1) == -1) {
+        perror("semop lock failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Function to unlock semaphore (V operation)
+void sem_Signal(int semid,short unsigned int sem_num) {
+    struct sembuf sb = {sem_num, 1, 0}; // Increment semaphore sem_num
+    if (semop(semid, &sb, 1) == -1) {
+        perror("semop unlock failed");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 6) { // Check for the correct number of arguments
@@ -14,12 +57,10 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    string commodity = argv[1];       // First argument (e.g., NATURALGAS)
-    if(commodity.length() > 10){
-        cerr << "Commodity name can not be more than 10 characters\n";
-        return 2;
-    }
-
+    char commodity[11];
+    strncpy(commodity, argv[1], sizeof(commodity) - 1); // First argument (e.g., NATURALGAS)
+    commodity[sizeof(commodity) - 1] = '\0'; // Ensure null-termination       
+    
     double price_mean = atof(argv[2]);         // Convert second argument to double
     double price_sd = atof(argv[3]);           // Convert third argument to double
     int wait_interval = std::atoi(argv[4]);     // Convert fourth argument to int
@@ -33,28 +74,69 @@ int main(int argc, char *argv[]) {
     cout << "Interval: " << wait_interval << " ms\n";
     cout << "Buffer size: " << buffer_size << "\n";
     
+    // creating the semaphore set for ONE PRODUCT--- for now iog
+    
+    // creating unique semkey -> 
+    // to access semaphore across different processes and such
+    key_t SEM_KEY = ftok("shmfile", 65);
+    // Create a semaphore set with 3 semaphores
+    int semid = semget(SEM_KEY, 3, IPC_CREAT | 0666);
+    if (semid == -1) {
+        perror("semget failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize semaphore 0 (mutex-access on buffer) to 1 (resource available)
+    if (semctl(semid, 0, SETVAL, 1) == -1) {
+        perror("semctl failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize semaphore 1 (empty slots) to buffer_size (resource available)
+    if (semctl(semid, 1, SETVAL, buffer_size) == -1) {
+        perror("semctl failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize semaphore 2 (full slots) to 0 (wait)
+    if (semctl(semid, 2, SETVAL, 0) == -1) {
+        perror("semctl failed");
+        exit(EXIT_FAILURE);
+    }
+
+
     default_random_engine generator;
     normal_distribution<double> distribution(price_mean,price_sd);
     // first let's try and produce into terminal instead into a prod log file
     while(true)
     {
-        struct timespec tp;
-        if (clock_gettime(CLOCK_REALTIME, &tp) == -1) {
-            cerr << "Problem with retrieving time.\n";
-            return 3;
-        }
+        print_time();
+        double price_gen = distribution(generator);
+        printf("%s: generating a new value %f\n", commodity, price_gen);
+        // produce
 
-        time_t timestamp = time(NULL);
-        struct tm datetime = *localtime(&timestamp);
-        char output[50];
-        long milliseconds = tp.tv_nsec / 1000000; // turn ns to ms
+        print_time();
+        printf("%s: trying to get mutex on shared buffer\n",commodity);
+        // semWait(e) -> mt spot?
+        //sem_Wait(semid,1);
 
-        // Format the date and time string
-        strftime(output, 50, "%m/%d/%Y %H:%M:%S", &datetime);
+        print_time();
+        printf("%s: placing %f on shared buffer\n",commodity,price_gen);
+        // semWait(s) -> mutex
+        //sem_Wait(semid,0);
 
-        // Print using printf
-        printf("[%s.%03ld] ", output, milliseconds); // 0 padding
-        printf("Price generated: %f\n",distribution(generator));
+        // append
+
+        // semSignal(s) -> mutex
+        //sem_Signal(semid,0);
+
+        // smeSignal(n) -> a product is ready
+        //sem_Signal(semid,2);
+        
+        
+        print_time();
+        printf("%s: sleeping for %d ms\n", commodity,wait_interval); 
+        
         usleep(wait_interval * 1000); // usleep uses micro and we got milli
     }
     
